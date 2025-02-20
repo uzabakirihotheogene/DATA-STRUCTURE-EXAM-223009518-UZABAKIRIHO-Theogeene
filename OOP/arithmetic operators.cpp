73dabac5501b40e0b3bc4978c2e4c649efdc8d97
uzabakirihotheogene <uzabakirihotheogene001@gmail.com>
2025-02-20 17:27:29 +0200

    arithmetic operators.cpp

---
+++ OOP/arithmetic operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a = 10, b = 3;
+cout << "Sum: " << a + b << endl;
+cout << "Difference: " << a - b << endl;
+cout << "Product: " << a * b << endl;
+cout << "Quotient: " << a / b << endl;
+cout << "Remainder: " << a % b << endl;
+
+return 0;
+}
